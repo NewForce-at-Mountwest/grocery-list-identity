3188d1c89bf4b6892aeba5c3cfd5c8c3a3fe57de
Jordan Castelloe <jordan.castelloe@gmail.com>
2019-06-12 16:18:10 -0400

    added seed data and put full name on login

--- IdentityPractice/Data/ApplicationDbContext.cs
+++ IdentityPractice/Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using IdentityPractice.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,11 +13,52 @@ namespace IdentityPractice.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-            
+
         }
 
         public DbSet<ListItem> ListItem { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Create a new user for Identity Framework
+            ApplicationUser user = new ApplicationUser
+            {
+                Id = "1",
+                FirstName = "admin",
+                LastName = "admin",
+                UserName = "admin@admin.com",
+                NormalizedUserName = "ADMIN@ADMIN.COM",
+                Email = "admin@admin.com",
+                NormalizedEmail = "ADMIN@ADMIN.COM",
+                EmailConfirmed = true,
+                LockoutEnabled = false,
+                SecurityStamp = Guid.NewGuid().ToString("D")
+            };
+            var passwordHash = new PasswordHasher<ApplicationUser>();
+            user.PasswordHash = passwordHash.HashPassword(user, "Admin8*");
+            modelBuilder.Entity<ApplicationUser>().HasData(user);
+
+            // Create two cohorts
+            modelBuilder.Entity<ListItem>().HasData(
+                new ListItem()
+                {
+                    Id=1,
+                    Name = "Bologna",
+                    UserId = "1"
+                },
+                new ListItem()
+                {
+                    Id=2,
+                    Name = "Peanut butter",
+                    UserId = "1"
+                }
+            );
+
+
+
+
+        }
     }
 }
---
+++ IdentityPractice/Data/Migrations/20190612195837_seed-data.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentityPractice.Data.Migrations
+{
+    public partial class seeddata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Discriminator", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName", "FirstName", "LastName" },
+                values: new object[] { "1", 0, "6af112f7-e8de-46c2-a1d0-c1665aabb56c", "ApplicationUser", "admin@admin.com", true, false, null, "ADMIN@ADMIN.COM", "ADMIN@ADMIN.COM", "AQAAAAEAACcQAAAAEGDBJdmOcVZ3FB0E5AxiznGJraW3RJ4dRG9qDDJUCeQZr57UG7ubmg1gZ+YW1tcUug==", null, false, "ddf143d7-a8d6-4479-95d1-4f060c49b407", false, "admin@admin.com", "admin", "admin" });
+
+            migrationBuilder.InsertData(
+                table: "ListItem",
+                columns: new[] { "Id", "Name", "UserId" },
+                values: new object[] { 1, "Bologna", "1" });
+
+            migrationBuilder.InsertData(
+                table: "ListItem",
+                columns: new[] { "Id", "Name", "UserId" },
+                values: new object[] { 2, "Peanut butter", "1" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ListItem",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ListItem",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1");
+        }
+    }
+}
